98ec6678e1f931500e2739e958511705f9e1a5a8
jakubwojcik <jakubwojcik.post@gmail.com>
2018-05-13 20:38:06 +0200

    Adding more user information to user model.

--- src/main/java/main/Application.java
+++ src/main/java/main/Application.java
@@ -1,6 +1,5 @@
 package main;
 
-import dao.DatabaseAdapter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -12,7 +11,6 @@ import org.springframework.context.annotation.ComponentScan;
 public class Application {
 
     public static void main(String[] args) {
-        DatabaseAdapter db = new DatabaseAdapter();
         SpringApplication.run(Application.class, args);
     }
 }
