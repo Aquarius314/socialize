b279f3cbe8e53d71b903ab208ff1ebd20d2a9152
Jakub Syrek <jksyrek@gmail.com>
2018-05-15 22:28:12 +0200

    first changes to visual

--- src/main/java/main/Application.java
+++ src/main/java/main/Application.java
@@ -1,5 +1,6 @@
 package main;
 
+import dao.DatabaseAdapter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class Application {
 
     public static void main(String[] args) {
+        DatabaseAdapter db = new DatabaseAdapter();
         SpringApplication.run(Application.class, args);
     }
 }
